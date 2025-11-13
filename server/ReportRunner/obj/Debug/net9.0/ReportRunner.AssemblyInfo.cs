7bcf948db792f1f9240b15c2e5cb3f35ae97e31d
nicholas <aigberuan6@gmail.com>
2025-11-13 14:45:35 +0100

    upd1

--- server/ReportRunner/obj/Debug/net9.0/ReportRunner.AssemblyInfo.cs
+++ server/ReportRunner/obj/Debug/net9.0/ReportRunner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReportRunner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d83ce8a5f4823976c927ef26d70fffdf4c53755a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47493197b03b24da2df15c36e43c755b9d388774")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReportRunner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReportRunner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
